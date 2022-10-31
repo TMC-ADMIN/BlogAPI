01b4e93fe7f75cdd644b4b3316b818490551149c
JP Lavalley <jplavalley@mtgcoop.com>
2022-10-31 13:22:06 -0400

    Update and Delete Button

--- BlogAPI/urls.py
+++ BlogAPI/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', include('backend.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
 ]
