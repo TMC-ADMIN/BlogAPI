3ec1125c71731121a6a942384eb436f8366ca71c
JP Lavalley <jplavalley@mtgcoop.com>
2022-10-31 10:54:50 -0400

    did login stuff

--- BlogAPI/settings.py
+++ BlogAPI/settings.py
@@ -84,7 +84,9 @@ TEMPLATES = [
 WSGI_APPLICATION = "BlogAPI.wsgi.application"
 
 CORS_ALLOWED_ORIGINS = [
-    "https://localhost:3000",
+    "https://localhost:2000",
+    "http://127.0.0.1:2000",
+
 ]
 
 
